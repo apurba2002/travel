72c6e7188d67df3fd67b1874d3e02937609024f4
apurba2002 <debnathapurba582@gmail.com>
2023-01-02 22:06:58 +0600

    Initial commit

---
+++ assests/main.js
@@ -0,0 +1,18 @@
+
+document.getElementById('home').onclick=function(){
+    window.location.href="index.html";
+}
+document.getElementById('name').onclick=function(){
+    window.location.href="index.html";
+}
+document.getElementById('travel').onclick=function(){
+    window.location.href="travels.html";
+}
+document.getElementById('company').onclick=function(){
+    window.location.href="about.html";
+}
+
+
+
+
+
